9363bc6958cd83f06a09745d65d5c07f67c2f47b
Serhii Piddubchak <serhii.piddubchak@gmail.com>
2012-01-18 00:28:54 +0200

    downsample: fix handling of filenames with spaces

--- downsample_flac.sh
+++ downsample_flac.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 IN=$1
-IN_SR=`metaflac --show-sample-rate $1`
-IN_BPS=`metaflac --show-bps $1`
+IN_SR=`metaflac --show-sample-rate "$1"`
+IN_BPS=`metaflac --show-bps "$1"`
 
 OUT=$2
 OUT_SR=44100
 OUT_BPS=16
 
-TAGS=$OUT.tags
+TAGS="$OUT.tags"
 
 #cuesheet section in flac file doesn't include track names rendering it useless
 #CUESHEET=${IN%.flac}.cue
@@ -38,22 +38,22 @@ TAGS=$OUT.tags
 #Option 4: always works and almost as fast as #1
 #the only slowdown is in additional seektable generation command
 echo "Decoding, downsampling and encoding"
-flac -s -d -c --force-raw-format --endian=little --sign=signed $IN |\
+flac -s -d -c --force-raw-format --endian=little --sign=signed "$IN" |\
 sox -t raw -r $IN_SR -b $IN_BPS -c 2 -e signed-integer -\
     -D\
     -t raw -r $OUT_SR -b $OUT_BPS - |\
-flac -s -8 -o $OUT -P 65536 --endian=little --channels=2 --sign=signed --bps=$OUT_BPS --sample-rate=$OUT_SR -
+flac -s -8 -o "$OUT" -P 65536 --endian=little --channels=2 --sign=signed --bps=$OUT_BPS --sample-rate=$OUT_SR -
 
 echo "Adding seektable"
-metaflac --add-seekpoint=4s $OUT
+metaflac --add-seekpoint=4s "$OUT"
 
 echo "Exporting tags"
-metaflac --export-tags-to=$TAGS $IN
+metaflac --export-tags-to="$TAGS" "$IN"
 
 echo "Importing tags"
-metaflac --import-tags-from=$TAGS $OUT
+metaflac --import-tags-from="$TAGS" "$OUT"
 
-rm $TAGS
+rm "$TAGS"
 echo "Downsampling finished"
-ls -lh $OUT
+ls -lh "$OUT"
 
